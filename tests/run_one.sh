81c4dbb932965ed7a02ac961210a532902bc7bfd
Restray <contact@restray.org>
2022-05-31 11:31:47 +0200

    Update sudo and path on tests

--- tests/run_one.sh
+++ tests/run_one.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -e
 
-(cd $1 ; ./server.sh ; ./client.sh) > $1.log
+(cd $1 ; sudo ./server.sh ; ./client.sh) > $1.log
--- tests/test-empty-body-length/server.sh
+++ tests/test-empty-body-length/server.sh
@@ -2,8 +2,8 @@
 
 docker rm --force nginx-test-server || true
 docker run --name nginx-test-server \
-            -v "/mnt/d/Programmation/Docker/webserv/test-empty-body-length/nginx.conf:/etc/nginx/nginx.conf:ro" \
-            -v "/mnt/d/Programmation/Docker/webserv/test-empty-body-length/static:/www/data/" \
+            -v "$PWD/nginx.conf:/etc/nginx/nginx.conf:ro" \
+            -v "$PWD/static:/www/data/" \
             -p 8080:80 \
             -d \
             nginx
